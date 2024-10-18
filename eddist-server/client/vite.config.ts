1f4a56e80b075492dbbcc8223c891e6bd7a5571e
edginer <155087192+edginer@users.noreply.github.com>
2024-10-19 05:08:36 +0900

    update vite setting

--- eddist-server/client/vite.config.ts
+++ eddist-server/client/vite.config.ts
@@ -4,6 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
