ca0124f91cdde539fb69038d95ddb183f6228d26
edginer <155087192+edginer@users.noreply.github.com>
2025-06-01 18:39:54 +0900

    fix: Rename AVAILABLE_USER_REGISTRATION to ENABLE_USER_REGISTRATION for consistency

--- eddist-server/client-v2/server/app.ts
+++ eddist-server/client-v2/server/app.ts
@@ -6,7 +6,7 @@ declare module "react-router" {
   interface AppLoadContext {
     EDDIST_SERVER_URL: string;
     BBS_NAME: string;
-    AVAILABLE_USER_REGISTRATION: boolean;
+    ENABLE_USER_REGISTRATION: boolean;
   }
 }
 
@@ -14,8 +14,8 @@ export const app = express();
 const EDDIST_SERVER_URL =
   process.env.EDDIST_SERVER_URL ?? "http://localhost:8080";
 const BBS_NAME = process.env.BBS_NAME ?? "エッヂ掲示板";
-const AVAILABLE_USER_REGISTRATION =
-  process.env.AVAILABLE_USER_REGISTRATION === "true";
+const ENABLE_USER_REGISTRATION =
+  process.env.ENABLE_USER_REGISTRATION === "true";
 
 app.use(
   createRequestHandler({
@@ -23,7 +23,7 @@ app.use(
     getLoadContext: () => ({
       EDDIST_SERVER_URL: EDDIST_SERVER_URL,
       BBS_NAME: BBS_NAME,
-      AVAILABLE_USER_REGISTRATION: AVAILABLE_USER_REGISTRATION,
+      ENABLE_USER_REGISTRATION: ENABLE_USER_REGISTRATION,
     }),
   })
 );
